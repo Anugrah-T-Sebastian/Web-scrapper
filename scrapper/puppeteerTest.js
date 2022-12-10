84be97fce0dbd953ca0e316d75a174feb41bec83
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2022-12-10 18:52:47 +0530

    Getting URL, URL Text & application info from search results

--- scrapper/puppeteerTest.js
+++ scrapper/puppeteerTest.js
@@ -15,31 +15,48 @@ const start = async (url) => {
     path: `../screenshots/start-pages/startPages${timeStamp}.png`,
   });
 
-  //console.log(references);
-  //   const searchField = await page.evaluate(() => {
-  //     const testEl = document.querySelector(".row3");
-  //     return testEl.innerHTML;
-  //   });
-
-  await page.type("#simpleSearchString", "listed demolition");
-  //   await page.click(
-  //     "#simpleSearchForm > div.row3 > input.button.primary.recaptcha-submit"
-  //   );
-  //   page.waitForNavigation();
-  await Promise.all([
-    page.click(
-      "#simpleSearchForm > div.row3 > input.button.primary.recaptcha-submit"
-    ),
-    page.waitForNavigation(),
-  ]);
-  const timeStamp2 = +new Date();
-  console.log("Search TIME:", timeStamp2);
-  await page.screenshot({
-    path: `../screenshots/search-pages/searchResults${timeStamp2}.png`,
-  });
-
   //console.log(test);
 
+  try {
+    await page.type("#simpleSearchString", "listed demolition");
+    //   await page.click(
+    //     "#simpleSearchForm > div.row3 > input.button.primary.recaptcha-submit"
+    //   );
+    //   page.waitForNavigation();
+    await Promise.all([
+      page.click(
+        "#simpleSearchForm > div.row3 > input.button.primary.recaptcha-submit"
+      ),
+      page.waitForNavigation(),
+    ]);
+    const timeStamp2 = +new Date();
+    console.log("Search TIME:", timeStamp2);
+    await page.screenshot({
+      path: `../screenshots/search-pages/searchResults${timeStamp2}.png`,
+    });
+  } catch (error) {
+    console.error("Time out error. Try again after sometime");
+  }
+
+  const { linksList, linksText, linkRefNo } = await page.evaluate(() => {
+    const linkElementsList = document.querySelectorAll(".searchresult");
+    let linkRefNo = [];
+    let linksList = [];
+    let linksText = [];
+    linkElementsList.forEach((link) => {
+      linksList.push(link.querySelector("a").href);
+      linkRefNo.push(link.querySelector(".metaInfo").innerText.trim());
+      linksText.push(link.querySelector("a").innerText);
+    });
+
+    return { linksList, linksText, linkRefNo };
+  });
+
+  console.log("Ref. No.", linkRefNo);
+  console.log("Links List", linksList);
+  console.log("Links Text:", linksText);
+
+  // BROWSER CLOSE
   await browser.close();
 };
 
